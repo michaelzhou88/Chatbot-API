c5a41944d78833b37ffeae14d90aafbcac4985fc
Michael Zhou <zhoumichael8@gmail.com>
2022-08-13 13:59:57 +0100

    Added Footer section to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Header.js";
 import Main from "./Main.js";
+import Footer from "./Footer.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Main /> 
+      <Main id="content"/> 
+      <Footer id="footer"/>
     </div>
   );
 }
---
+++ frontend/src/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+function Footer() {
+  return (
+    <footer class="py-5 my-5 bg-dark">
+        <Container className="px-4">
+          <p class="text-center text-white">
+            Copyright &copy; Sleep Hygiene Bot 2022
+          </p>
+        </Container>
+    </footer>
+)
+}
+
+export default Footer

7e3635c502da3d0b1d52183a05b303155d2c6443
Michael Zhou <zhoumichael8@gmail.com>
2022-08-13 13:12:32 +0100

    Modularised Header file and added responsive grid layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,14 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import './App.css';
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Header.js";
+import Main from "./Main.js";
 
 function App() {
   return (
     <div className="App">
-      <header>
-          <Navbar bg="light" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">SleepBot</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-      </header>
+      <Header />
+      <Main /> 
     </div>
   );
 }
